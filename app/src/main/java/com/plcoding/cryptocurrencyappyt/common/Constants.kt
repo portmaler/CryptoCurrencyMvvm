07711524e46c9c2d8d7d296ae104e25934d965bc
portmaler <56043999+portmaler@users.noreply.github.com>
2022-08-13 18:12:33 +0100

    first branch1 commit

--- app/src/main/java/com/plcoding/cryptocurrencyappyt/common/Constants.kt
+++ app/src/main/java/com/plcoding/cryptocurrencyappyt/common/Constants.kt
@@ -1,4 +1,8 @@
 package com.plcoding.cryptocurrencyappyt.common
 
 object Constants {
+
+    const val BASE_URL = "hhtps://api.coinpaprika.com/"
+    const val PARAM_COIN_ID = "coinId" +
+            ""
 }
